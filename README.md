228438c9e0366f2b800285a18518677a30f9fc6e
Sunny Jiao <sunnyjiao888@gmail.com>
2023-09-21 02:35:51 -0400

    working start/stop/delete, working on data persistence

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mc-server-manager
 Minecraft self-hosting utility
+
+# Planned Features
+- Create Vanilla, Spigot & Bukkit servers
+- Customize all itzg/minecraft-server environment variables
+- Replace worlds and other server files
+- Remote RCON access
+- Auto-backups
+- Real-time analytics and metrics
--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from kubernetes import client, config
+
+import sys
+
+from uuid import uuid4
 	
 config.load_incluster_config()
+
 k8s_core_v1 = client.CoreV1Api()
 k8s_apps_v1 = client.AppsV1Api()
 
@@ -11,34 +16,66 @@ app = Flask(__name__)
 def hello():
     return "Hello from Python!"
 
+
 @app.route("/list")
 def get_pods_list():
     pods_list = k8s_core_v1.list_namespaced_pod(namespace='default')
-    pods = [item.metadata.name for item in pods_list.items]
+    pods = [{"name": item.metadata.name, "labels": item.metadata.labels} for item in pods_list.items]
 
     return pods
+
 
 @app.route("/fetch")
 def fetch():
     pass
 
-@app.route("/stop-server/<id>")
-def stop_server():
-    pass
 
-@app.route("/create-server")
+@app.route("/delete-server/<server_id>")
+def delete_server(server_id):
+
+    try:
+        # Find deployment with the id
+        deployment_list = k8s_apps_v1.list_namespaced_deployment("default", label_selector=f"server-id={server_id}")
+        print(deployment_list.items, file=sys.stderr)
+        
+        for deployment in deployment_list.items:
+            deployment_name = deployment.metadata.name
+
+            # Delete the Deployment
+            k8s_apps_v1.delete_namespaced_deployment(
+                name=deployment_name,
+                namespace="default",
+                body=client.V1DeleteOptions(
+                    propagation_policy="Foreground",  # Specify deletion policy
+                    grace_period_seconds=5  # Specify grace period before deletion
+                )
+            )
+        
+        # Delete the service for the pod
+        k8s_core_v1.delete_namespaced_service(name=f"mc-svc-{server_id}", namespace="default")
+
+        return jsonify({'message': f'Deleted server with identifier {server_id}'}), 200
+    except Exception as e:
+        return jsonify({'error': f'Error deleting server {server_id}: {e}'}), 500
+
+
+@app.post("/create-server")
 def create_server():
-    e_eula = client.V1EnvVar(name="EULA", value="TRUE")
+
+    request_data = request.get_json()
+    print(request_data, file=sys.stderr)
 
     # Apply the Deployment to the cluster
     try:
+        server_id = str(uuid4())
+
         # Create service to access the MC server port
         k8s_core_v1.create_namespaced_service(
             namespace="default",
             body=client.V1Service(
                 api_version="v1",
                 kind="Service",
-                metadata=client.V1ObjectMeta(name="mc-server-service"),
+                metadata=client.V1ObjectMeta(name=f"mc-svc-{server_id}"),
                 spec=client.V1ServiceSpec(
                     type="LoadBalancer",
                     selector={"app": "minecraft-server"},  # Select the pods to expose
@@ -47,12 +84,68 @@ def create_server():
             )
         )
 
+        # RETURN NTOES: Must specify StorageClass otherwise it defaults to some weird folder in the Docker Desktop VM
+
+        # Create a persistent volume claim (PVC)
+        k8s_core_v1.create_namespaced_persistent_volume_claim(
+            namespace="default",
+            body=client.V1PersistentVolumeClaim(
+                api_version="v1",
+                kind="PersistentVolumeClaim",
+                metadata=client.V1ObjectMeta(name=f"pvc-{server_id}"),
+                spec=client.V1PersistentVolumeClaimSpec(
+                    access_modes=["ReadWriteMany"],
+                    resources=client.V1ResourceRequirements(
+                        requests={"storage": "30Gi"}  # TODO make storage configurable
+                    ),
+                ),
+            )
+        )
+        
+        # Create a persistent volume (PV) on the host
+        k8s_core_v1.create_persistent_volume(
+            client.V1PersistentVolume(
+                api_version="v1",
+                kind="PersistentVolume",
+                metadata=client.V1ObjectMeta(name=f"pv-{server_id}"),
+                spec=client.V1PersistentVolumeSpec(
+                    capacity={"storage": "10Gi"},  # Adjust storage capacity as needed
+                    volume_mode="Filesystem",  # Set to "Block" for raw block devices
+                    access_modes=["ReadWriteMany"],  # Adjust access mode as needed
+                    persistent_volume_reclaim_policy="Retain",  # Adjust reclaim policy as needed
+                    host_path=client.V1HostPathVolumeSource(path=f"/Users/sunnyjiao/mc_servers/{server_id}/"),  # Specify the host path
+                ),
+            )
+        )
+
+        # Configure itzg/minecraft-server container
+        env_list = []
+        env_list.append(client.V1EnvVar(name="EULA", value="TRUE")) # Mandatory EULA env var
+        for k, v in request_data.items():
+            env_list.append(client.V1EnvVar(name=k, value=v))
+
+        itzg_container = client.V1Container(
+            name="minecraft-server-container",
+            image="itzg/minecraft-server",
+            ports=[client.V1ContainerPort(container_port=25565)],
+            env=env_list,
+            volume_mounts=[client.V1VolumeMount(
+                name=f"volume-{server_id}",
+                mount_path="/data"
+            )]
+        )
+        #/Users/sunnyjiao/mc_servers/{server_id}/:data
+
+        # Create the deployment
         k8s_apps_v1.create_namespaced_deployment(
             namespace="default",  # Replace with the desired namespace
             body=client.V1Deployment(
                 api_version="apps/v1",
                 kind="Deployment",
-                metadata=client.V1ObjectMeta(name="mc-server-1"),
+                metadata=client.V1ObjectMeta(
+                    name=f"mc-server-{server_id}",
+                    labels={"server-id": server_id}
+                ),
                 spec=client.V1DeploymentSpec(
                     replicas=1,
                     selector=client.V1LabelSelector(
@@ -60,26 +153,61 @@ def create_server():
                     ),
                     template=client.V1PodTemplateSpec(
                         metadata=client.V1ObjectMeta(
-                            labels={"app": "minecraft-server"}
+                            labels={"app": "minecraft-server", "server-id": server_id}
                         ),
                         spec=client.V1PodSpec(
-                            containers=[client.V1Container(
-                                name="minecraft-server-container",
-                                image="itzg/minecraft-server",
-                                ports=[client.V1ContainerPort(container_port=25565)],
-                                env=[e_eula]
-                            )]
+                            containers=[itzg_container],
+                            volumes=[
+                                client.V1Volume(
+                                    name=f"volume-{server_id}",  # Name for the volume
+                                    persistent_volume_claim=client.V1PersistentVolumeClaimVolumeSource(claim_name=f"pvc-{server_id}"),  # Reference to the PVC
+                                )
+                            ]
                         )
                     )
                 )
             )
         )
-        print("Deployment created successfully!")
-        return "Success"
+        print("Deployment created successfully!", file=sys.stderr)
+        return jsonify({'message': f"Successfully created server with id: {server_id}", 'server_id': server_id}), 201
     except Exception as e:
-        print(f"Error creating Deployment: {e}")
-        return str(e)
+        return jsonify({"message": f"Error creating Deployment: {e}"}), 500
 
+
+@app.get("/stop-server/<server_id>")
+def stop_server(server_id):
+    try:
+        # Get the current Deployment object
+        deployment = k8s_apps_v1.read_namespaced_deployment(name=f"mc-server-{server_id}", namespace="default")
+        # Update the desired replica count to 0
+        deployment.spec.replicas = 0
+        # Apply the updated Deployment object to the cluster
+        k8s_apps_v1.replace_namespaced_deployment(name=f"mc-server-{server_id}", namespace="default", body=deployment)
+
+        return jsonify({'message': f"Successfully stopped server with id: {server_id}", 'server_id': server_id}), 200
+    except client.rest.ApiException as e:
+        if e.status == 404:
+            return jsonify({'message': f"Deployment 'mc-server-{server_id}' not found in namespace 'default'."}), 404
+        else:
+            return jsonify({'message': f"Error scaling down Deployment 'mc-server-{server_id}' in namespace 'default': {e}"}), 500
+
+
+@app.get("/start-server/<server_id>")
+def start_server(server_id):
+    try:
+        # Get the current Deployment object
+        deployment = k8s_apps_v1.read_namespaced_deployment(name=f"mc-server-{server_id}", namespace="default")
+        # Update the desired replica count to 1
+        deployment.spec.replicas = 1
+        # Apply the updated Deployment object to the cluster
+        k8s_apps_v1.replace_namespaced_deployment(name=f"mc-server-{server_id}", namespace="default", body=deployment)
+        
+        return jsonify({'message': f"Successfully started server with id: {server_id}", 'server_id': server_id}), 200
+    except client.rest.ApiException as e:
+        if e.status == 404:
+            print(f"Deployment 'mc-server-{server_id}' not found in namespace 'default'."), 404
+        else:
+            print(f"Error scaling up Deployment 'mc-server-{server_id}' in namespace 'default': {e}"), 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
